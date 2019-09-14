95f1c851877a37ff9bf5c2a251fa3929be0873ff
dea96 <hariskaefendi1996@gmail.com>
2019-09-14 22:14:39 +0700

    add assosiations between user and articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_13_134946) do
+ActiveRecord::Schema.define(version: 2019_09_14_141704) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
